59b6b4c75b60cd16b6863b9f832af16684e74779
vivek <vivek.kp.46@gmail.com>
2025-07-18 00:13:47 +0530

    debug

--- Diary/settings.py
+++ Diary/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zze7r+hfpbkt5-@!uo1u-#bp8a__6olo8y+wrelvre37zvi7d0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
